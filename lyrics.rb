57ef64fe76996c18b8c65233986b3557eb33b125
Collin Lefeber <lefeberc@gmail.com>
2012-10-27 01:01:41 -0400

    filtering lyrics

--- lyrics.rb
+++ lyrics.rb
@@ -14,5 +14,32 @@ get '/search/:query' do
   html = open(uri)
   doc = Nokogiri.HTML(html)
   doc.css('style, script, img, objecft, embed, iframe, canvas').remove
-  doc.css('#search')[0].to_s
+  search = doc.css('#search')[0]
+  # nice end for a lite search
+  # return search.to_s
+  links = []
+  search.css('li.g h3 a').each do |link|
+    puts link.text
+    text = clean_text(link.text)
+    puts text
+    links << remove_lyrics(text)
+  end
+  links.to_s
+end
+
+def clean_text(str)
+  # remove cruddy characters
+  encoding_options = {
+    :invalid           => :replace,  # Replace invalid byte sequences
+    :undef             => :replace,  # Replace anything not defined in ASCII
+    :replace           => '',        # Use a blank for those replacements
+    :universal_newline => true       # Always break lines with \n
+  }
+  str.encode!(Encoding.find('ASCII'), encoding_options)
+end
+
+def remove_lyrics(str)
+  str = str.gsub(/song\s*freak\.com/i, '')
+  str = str.gsub(/metro\s*lyric[s]?/i, '')
+  str = str.gsub(/lyric[s]/i, '')
 end
