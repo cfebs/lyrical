69d9180f7861d7d75efe4c20046431a2b7b65b0a
Collin Lefeber <lefeberc@gmail.com>
2012-10-27 21:53:31 -0400

    organize a bit

--- lyrics.rb
+++ lyrics.rb
@@ -9,8 +9,36 @@ get '/' do
 end
 
 get '/search/:query' do
-  q = URI.escape(params[:query])
-  uri = "http://www.google.com/search?q=#{q}"
+  uri = prepare_uri(params[:query])
+  links = get_google_links(uri)
+  links.to_s
+end
+
+##
+# remove common lyric site names
+##
+def remove_lyrics(str)
+  str = str.gsub(/song\s*freak\.com/i, '')
+  str = str.gsub(/metro\s*lyric[s]?/i, '')
+  str = str.gsub(/lyric[s]/i, '')
+end
+
+##
+# prepares a google uri
+##
+def prepare_uri(str)
+  if !(/lyric[s]/i.match(str))
+    str = "#{str} lyrics"
+  end
+
+  str = URI.escape(str)
+  return "http://www.google.com/search?q=#{str}"
+end
+
+##
+# Opens the google search page and gets the h3 links
+##
+def get_google_links(uri)
   html = open(uri)
   doc = Nokogiri.HTML(html)
   doc.css('style, script, img, objecft, embed, iframe, canvas').remove
@@ -19,16 +47,17 @@ get '/search/:query' do
   # return search.to_s
   links = []
   search.css('li.g h3 a').each do |link|
-    puts link.text
     text = clean_text(link.text)
-    puts text
     links << remove_lyrics(text)
   end
-  links.to_s
+
+  return links
 end
 
+##
+# remove cruddy characters in link text
+##
 def clean_text(str)
-  # remove cruddy characters
   encoding_options = {
     :invalid           => :replace,  # Replace invalid byte sequences
     :undef             => :replace,  # Replace anything not defined in ASCII
@@ -36,10 +65,4 @@ def clean_text(str)
     :universal_newline => true       # Always break lines with \n
   }
   str.encode!(Encoding.find('ASCII'), encoding_options)
-end
-
-def remove_lyrics(str)
-  str = str.gsub(/song\s*freak\.com/i, '')
-  str = str.gsub(/metro\s*lyric[s]?/i, '')
-  str = str.gsub(/lyric[s]/i, '')
 end
